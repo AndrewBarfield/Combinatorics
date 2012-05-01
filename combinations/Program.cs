3efe299f98bd0874f5578a82fbf5eb1e3df005ef
Andrew Barfield <barfield2256@gmail.com>
2012-05-01 08:25:04 -0400

    first commit

---
+++ combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace combinations
+{
+        class Program
+        {
+                static void Main( string[] args )
+                {
+
+                        using ( StreamWriter file = new StreamWriter(
+                            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "combinations.csv"),
+                            false ) )
+                        {
+
+                                CombinatoricsUtilities.GetCombinations( 
+                                    new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, a => {
+                                    file.WriteLine("{0}, {1}, {2}, {3}, {4}", a[0], a[1], a[2], a[3], a[4]);
+                                }, 5, false );
+                        }
+
+                }
+        }
+}
+
+
